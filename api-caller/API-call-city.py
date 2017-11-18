f30bfd3ef04c3438e7f9b853fe136f72bccd3050
mojtaba-komeili <mojtaba.komeili@gmail.com>
2017-11-18 13:37:41 -0700

    Data clean up function added to the api caller

--- api-caller/API-call-city.py
+++ api-caller/API-call-city.py
@@ -10,11 +10,15 @@ EDMONTON = 'edmonton'
 # Data types
 FIRE = 'fire'
 EDUCATION = 'education'
+AIRQUALITY = 'air quality'
+AIRQUALITY_COLS = 'air quality columns' # Name of columns we use for air quality data
 
 # Data urls for each city
 calgary = {
     FIRE : "https://data.calgary.ca/resource/mqii-992y.csv",
-    EDUCATION : "https://data.calgary.ca/resource/gddc-smf3.csv"
+    EDUCATION : "https://data.calgary.ca/resource/gddc-smf3.csv",
+    AIRQUALITY : "https://data.calgary.ca/resource/88iq-yi9x.csv",
+    AIRQUALITY_COLS : ["station_name", "parameter", "date", "average_daily_value"]
 }
 
 edmonton = {
@@ -38,15 +42,30 @@ class API_Caller:
             df = pd.read_csv(url)
         except KeyError:
             print("%s data link doesn't exist for %s" %(type, city))
-            df = None
+            return
+
+        df = self.clean_df(df, city, type)
 
         return df
+
+    def clean_df(self, df, city, type):
+        try:
+            if type == AIRQUALITY:
+                columns = df.columns
+                columns_keep = cities[city][AIRQUALITY_COLS]
+                columns_del  = [c for c in columns if c not in columns_keep]
+                df = df.drop(columns_del, axis=1)
+        except KeyError:
+            print("The key for data clean up does not match the data set")
+
+        return df
+
 
 
 if __name__ == '__main__':
     print("Started testing")
     caller = API_Caller()
-    df = caller.get_data(EDMONTON, EDUCATION)
+    df = caller.get_data(CALGARY, AIRQUALITY)
     if df is not None:
         print(df.head(5))
 
